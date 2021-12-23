d46952a06786df764ec2489b7f429c4def605e58
Josh <96540832+joshuakearney140@users.noreply.github.com>
2021-12-23 14:03:09 +0000

    Add files via upload

---
+++ Length_Validation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Subroutine_Input_validation__length_validation_
+{
+    class Program
+    {
+        
+        
+        static void GetInputLengthCheck()
+        {
+            bool valid = false;
+            while (!valid)
+            {
+                Console.WriteLine("Enter password: ");
+                string password = Console.ReadLine();
+
+                if(password.Length < 8)
+                {
+                    valid = false;
+                }
+                else
+                {
+                    valid = true;
+                }
+            }           
+        }
+         
+        static void Main(string[] args)
+        {
+            GetInputLengthCheck();
+            Console.WriteLine("Valid password!");
+        }
+    }
+}
